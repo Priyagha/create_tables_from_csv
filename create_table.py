f36774d8f3b9fbf76a7f06c34bf2b1a410848cdd
Priya Patel <patelpriya19961996@gmail.com>
2023-10-13 16:01:43 -0500

    Generalizing loops to create tables

--- create_table.py
+++ create_table.py
@@ -12,3 +12,33 @@ files = sorted(glob('./*.csv'))
 
 # getting the current working directory
 pwd = os.getcwd()
+
+for file in files:
+    print(file)
+    df = pd.read_csv(file)
+    table_name = file.split('\\')[1].split('.')[0].upper()
+    table_str = f"CREATE TABLE {table_name} ("
+    for columns in df.columns:
+        obj_type = df[columns].dtype
+        obj_max = df[columns].apply(lambda x: len(str(x))).max()
+
+        if obj_type == object:
+            if columns in ['START', 'STOP', 'DATE'] or 'DATE' in columns:
+                table_str += f' {columns} DATE,'
+
+            elif obj_max == 36:
+                table_str += f' {columns} UUID,'
+
+            else:
+                table_str += f' {columns} VARCHAR({obj_max}),'
+
+        elif obj_type == int:
+            table_str += f' {columns} BIGINT,'
+        
+        elif obj_type == float:
+            table_str += f' {columns} REAL,'
+
+    table_str = table_str[:-1]
+    table_str += ');\n'
+
+    
